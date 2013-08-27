0389eba0f4205d0b19a29c7f031e583b777b6763
Alexey Kuznetsov <axet@me.com>
2013-08-27 10:31:04 +0400

    add exmpaple proto

---
+++ protonet-test/cpp.sh
@@ -0,0 +1,1 @@
+../protobuf/bin/protoc ExampleProto.proto --cpp_out=../protonet-objc-test-common/protonet-objc-test-common/
---
+++ protonet-test/java.sh
@@ -0,0 +1,1 @@
+../protobuf/bin/protoc ExampleProto.proto --java_out=../protonet-java-test-common/src/
